2147489c8cfb8c4a01ef1da35894c1fbcb9d242c
deFRAGerNZ <alex.vernel@gmail.com>
2013-11-12 10:15:28 +1300

    Update watchdog8.sh

--- watchdog8.sh
+++ watchdog8.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-# Tips welcome :) PYfnvSc9CRHCebGDtVkSijo6k7pCbpPthX
+#Tips welcome :) PYfnvSc9CRHCebGDtVkSijo6k7pCbpPthX
 
 function launchptsminer {
-        echo "######## Starting ptsminer... ########"
-        cd /$HOME/ptsminer/src
-        tsocks ./ptsminer -poolip=ptsmine.beeeeer.org -poolport=1337 -poolpassword=helloworldtest -pooluser=PZTygehAqwKXwUQyCGRYpk9z56HKAzwVmp -genproclimit=8 > ~/ptsminer.log &
+	echo "######## Starting ptsminer... ########"
+	cd /$HOME/ptsminer/src
+	./ptsminer -poolip=ptsmine.beeeeer.org -poolport=1337 -poolpassword=0 -pooluser=PZTygehAqwKXwUQyCGRYpk9z56HKAzwVmp -genproclimit=8 > ~/ptsminer.log &
 }
+
+control_c()
+{
+	echo "######## Stopping  ########"
+	ps aux | grep tail | grep ptsminer | awk  '{print "kill "$2}' | sh
+	killall ptsminer
+	exit 0
+}
+ 
+trap control_c SIGINT
+
+mv ~/ptsminer.log ~/ptsminer.log.old 2> /dev/null
 
 launchptsminer
 
@@ -14,15 +26,15 @@ tail -f ~/ptsminer.log &
 
 while true
 do
-        grep "force reconnect" ~/ptsminer.log
-        if [ $? -eq 0 ]
-        then
-                echo "######## Force reconnect DETECTED ########"
-                echo "######## Killing ptsminer... ########"
-                killall ptsminer
-                echo "######## Wait 5 secs... ########"
-                sleep 5s
-                launchptsminer
-        fi
-        sleep 1s
-donewatchdo
+	grep "force reconnect" ~/ptsminer.log
+	if [ $? -eq 0 ]
+	then
+		echo "######## Force reconnect DETECTED ########"
+		echo "######## Killing ptsminer... ########"
+		killall ptsminer
+		echo "######## Wait 5 secs... ########"
+		sleep 5s
+		launchptsminer
+	fi
+	sleep 1s
+done
